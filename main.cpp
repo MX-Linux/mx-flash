fe49eb23ff913994514b55bdbe18da93ac0ec11f
AdrianTM <AdrianTM@users.noreply.github.com>
2014-03-02 10:26:10 -0500

    change icon location

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-  a.setWindowIcon(QIcon("/usr/local/icons/32-Flash-manager.png"));
+  a.setWindowIcon(QIcon("/usr/share/icons/mx-flash.png"));
 
   QTranslator qtTran;
   qtTran.load(QString("qt_") + QLocale::system().name());
