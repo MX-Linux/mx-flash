75b3be5e893b7c700b4a522461236d76fa0999cd
anticapitalista <antiX@operamail.com>
2014-06-17 16:51:54 +0300

    Updated French translation

---
+++ translations/mx-flash_fr.ts
@@ -0,0 +1,264 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.0">
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="main.cpp" line="56"/>
+        <source>You must run this program as root.</source>
+        <translation>Vous devez lancer cette application avec les droits root.</translation>
+    </message>
+</context>
+<context>
+    <name>mxflash</name>
+    <message>
+        <location filename="mxflash.ui" line="20"/>
+        <location filename="mxflash.cpp" line="341"/>
+        <source>MX Flash Manager</source>
+        <translation>MX Gestionnaire Flash</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="47"/>
+        <source>About this application</source>
+        <translation>A propos de cette application</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="50"/>
+        <source>About...</source>
+        <translation>A propos...</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="57"/>
+        <source>Alt+B</source>
+        <translation>Alt+B</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="86"/>
+        <source>Display help </source>
+        <translation>Afficher l&apos;aide</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="89"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="96"/>
+        <source>Alt+H</source>
+        <translation>Alt+H</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="103"/>
+        <source>Apply any changes then quit</source>
+        <translation>Appliquer les modifications puis sortir</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="106"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="161"/>
+        <source>Cancel any changes then quit</source>
+        <translation>Annuler toutes les modifications puis sortir</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="164"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="171"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="213"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Sans'; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;justify&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Adobe Flash comes preinstalled on MX-14. However, it&apos;s a non-free program that has ongoing problems that concern some users. Consult the Help document for details.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;justify&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Adobe Flash est pré-installé sur MX-14. Cependant, c&apos;est un programme non-libre qui peut poser des problèmes à certains utilisateurs. Consultez le document d&apos;aide pour plus de détails&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="236"/>
+        <source>What would you like MX Flash Manager to do?</source>
+        <translation>Que souhaitez-vous que le MX Gestionnaire Flash fasse ?</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="248"/>
+        <source>Update Flash</source>
+        <translation>Mise à jour de Flash</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="255"/>
+        <source>This option (re)installs Flash, if needed will install non-sse2 Flash</source>
+        <translation>Cette option (ré) installe Flash, si nécessaire seront installé non-sse2 Flash</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="258"/>
+        <source>Reinstall Flash</source>
+        <translation>Réinstaller flash</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="265"/>
+        <source>Remove Flash</source>
+        <translation>Supprimer Flash</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="307"/>
+        <source>Removing Flash...</source>
+        <translation>Suppression de Flash ...</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="343"/>
+        <source>What kind of update do you prefer?</source>
+        <translation>Quel genre de mise à jour préférez-vous?</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="361"/>
+        <source>Select to update manually</source>
+        <translation>Sélectionnez pour mettre à jour manuellement</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="364"/>
+        <source>Update just one time</source>
+        <translation>Mettre à jour une seule fois</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="380"/>
+        <source>Select to set a daily scheduled update</source>
+        <translation>Sélectionnez pour installer une mise à jour programmée tous les jours</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="383"/>
+        <source>Update on an automatic schedule</source>
+        <translation>Mise à jour sur un calendrier automatique</translation>
+    </message>
+    <message>
+        <location filename="mxflash.ui" line="411"/>
+        <source>Installing Flash...</source>
+        <translation>Installation de Flash ...</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="87"/>
+        <source>Adobe Flash most recent version: </source>
+        <translation>Adobe Flash version la plus récente:</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="91"/>
+        <source>Adobe Flash installed version: </source>
+        <translation>Adobe Flash version installée:</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="93"/>
+        <source>Adobe Flash is not installed.</source>
+        <translation>Adobe Flash n&apos;est pas installé.</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="99"/>
+        <source>PepperFlash for Chrome version: </source>
+        <translation>PepperFlash pour Chrome version: </translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="102"/>
+        <source>PepperFlash for Chromium version: </source>
+        <translation>PepperFlash pour Chromium version: </translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="126"/>
+        <location filename="mxflash.cpp" line="133"/>
+        <location filename="mxflash.cpp" line="145"/>
+        <location filename="mxflash.cpp" line="157"/>
+        <location filename="mxflash.cpp" line="264"/>
+        <location filename="mxflash.cpp" line="278"/>
+        <location filename="mxflash.cpp" line="300"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="127"/>
+        <source>Could not remove </source>
+        <translation>Impossible de supprimer</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="134"/>
+        <source>Flash is not installed</source>
+        <translation>Flash n&apos;est pas installé</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="146"/>
+        <source>&lt;b&gt;Flash is not installed. Would you like to install it?&lt;/b&gt;</source>
+        <translation>&lt;b&gt; Flash n&apos;est pas installé. Voulez-vous l&apos;installer?&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="147"/>
+        <source>Yes</source>
+        <translation>Oui</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="147"/>
+        <source>No</source>
+        <translation>Non</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="158"/>
+        <source>Flash is installed through other means, cannot update with this program.</source>
+        <translation>Flash a été installé par d&apos;autres moyens, ne peut pas mettre à jour avec ce programme.</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="185"/>
+        <location filename="mxflash.cpp" line="256"/>
+        <source>Success</source>
+        <translation>Succès</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="186"/>
+        <source>An automatic daily update was scheduled. You can close the program now.&lt;p&gt;&lt;b&gt;Do you want to exit MX Flash Manager?&lt;/b&gt;</source>
+        <translation>Une mise à jour automatique a été prévue. Vous pouvez fermer le programme maintenant.&lt;p&gt;&lt;b&gt;Voulez-vous quitter le MX Gestionnaire Flash?&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="257"/>
+        <source>Process finished with success.&lt;p&gt;&lt;b&gt;Do you want to exit MX Flash Manager?&lt;/b&gt;</source>
+        <translation>Processus terminé avec succès. &lt;p&gt;&lt;b&gt; Voulez-vous quitter le MX Gestionnaire Flash?&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="265"/>
+        <source>Process finished. Errors have occurred.</source>
+        <translation>Processus terminé. Des erreurs sont survenues.</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="279"/>
+        <source>&quot;apt-get update&quot; command did not run successfully.</source>
+        <translation>La commande &quot;apt-get update&quot; n&apos;a pas été exécuté avec succès.</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="301"/>
+        <source>Could not download Flash for non-SSE2 CPUs.</source>
+        <translation>Impossible de télécharger Flash pour les non-SSE2 CPUs.</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="340"/>
+        <source>About MX Flash Manager</source>
+        <translation>À propos de MX Gestionnaire Flash</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="342"/>
+        <source>Simple Flash manager for antiX MX</source>
+        <translation>Simple Gestionnaire Flash pour antiX MX</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="343"/>
+        <source>Copyright (c) antiX</source>
+        <translation>Copyright (c) antiX</translation>
+    </message>
+    <message>
+        <location filename="mxflash.cpp" line="344"/>
+        <source>License</source>
+        <translation>License</translation>
+    </message>
+</context>
+</TS>
