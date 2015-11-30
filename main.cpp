adc3fd0d855cc3a46c173996db603bff76b89c35
Adrian <adrian@mxlinux.org>
2015-11-30 12:22:23 -0500

    new build translations, move app icon location

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon("/usr/share/pixmaps/mx/mx-flash.png"));
+    a.setWindowIcon(QIcon("/usr/share/pixmaps/mx-flash.png"));
 
     QTranslator qtTran;
     qtTran.load(QString("qt_") + QLocale::system().name());
